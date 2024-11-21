8864936bca72b5c4b5f76bb60ee73be854c0b384
Pantos <pantos+github@akk.org>
2024-11-21 19:27:53 +0100

    main: schöner wohnen

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,9 @@ fn main() -> eframe::Result {
         .with_inner_size([400.0, 300.0])
         .with_min_inner_size([300.0, 220.0])
         .with_icon(
-            egui::IconData::default(), // eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-                                       //     .expect("Failed to load icon"),
+            egui::IconData::default(),
+            // eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+            //     .expect("Failed to load icon"),
         );
 
     let native_options = eframe::NativeOptions {

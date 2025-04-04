5506a27ea625b7f825dac1e51322ff61eb07968d
Pantos <pantos+github@akk.org>
2025-04-04 02:28:41 +0200

    app: add copy char buttons

--- src/app.rs
+++ src/app.rs
@@ -68,7 +68,6 @@ impl eframe::App for App {
             ui.horizontal(|ui| {
                 ui.heading("DSA Hybrid Char Editor");
                 Self::quit_button(ui, ctx);
-                // egui::widgets::global_theme_preference_buttons(ui);
                 ui.with_layout(egui::Layout::top_down(egui::Align::LEFT), |ui| {
                     egui::widgets::global_theme_preference_buttons(ui);
                 });
@@ -87,10 +86,28 @@ impl eframe::App for App {
                         .striped(false)
                         .show(ui, |ui| {
                             ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
-                                let button =
+                                let copy_opponent_button = util::create_menu_button(
+                                    "⬅",
+                                    "Copy opponent to char",
+                                    40.0,
+                                    ui,
+                                );
+                                if copy_opponent_button.clicked() {
+                                    self.char = self.opponent.clone();
+                                }
+                                let switch_button =
                                     util::create_menu_button("↔", "Switch chars", 40.0, ui);
-                                if button.clicked() {
+                                if switch_button.clicked() {
                                     std::mem::swap(&mut self.char, &mut self.opponent);
+                                }
+                                let copy_char_button = util::create_menu_button(
+                                    "➡",
+                                    "Copy char to opponent",
+                                    40.0,
+                                    ui,
+                                );
+                                if copy_char_button.clicked() {
+                                    self.opponent = self.char.clone();
                                 }
                             });
                             ui.end_row();

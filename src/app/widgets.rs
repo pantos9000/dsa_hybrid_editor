52cc02ed21ccc8b75d9c6d593e2353d74f9cb738
Pantos <pantos+github@akk.org>
2025-08-13 20:42:07 +0200

    app/widgets: simplify mouseover help
    The `Tooltip` widget is actually supoerfluous.

--- src/app/widgets.rs
+++ src/app/widgets.rs
@@ -191,10 +191,8 @@ pub fn create_menu_button(text: &str, help: &str, size: f32, ui: &mut egui::Ui) 
     let text = egui::RichText::new(text).size(text_size);
     let button = egui::Button::new(text).corner_radius(10.0);
     ui.add_sized([size, size], button).on_hover_ui(|ui| {
-        egui::show_tooltip(ui.ctx(), ui.layer_id(), egui::Id::new("my_tooltip"), |ui| {
-            ui.horizontal(|ui| {
-                ui.label(help);
-            });
+        ui.horizontal(|ui| {
+            ui.label(help);
         });
     })
 }
